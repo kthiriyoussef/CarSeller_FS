1b59d0a1379fc064f63b3ac4ef70f02d9b9be54e
Kthiri Youssef <kthiriyoussef09@gmail.com>
2023-11-14 18:03:24 +0100

    last commit

---
+++ src/main/java/com/youssef/Miniprojet/security/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.youssef.Miniprojet.security;
+import java.util.Arrays;
+import java.util.Collections;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import
+org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception 
+	{ 		
+		 http.csrf().disable()
+		    .sessionManagement()
+	          .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+	          
+	          .cors().configurationSource(new CorsConfigurationSource() {
+	 			 @Override
+	 			 public CorsConfiguration getCorsConfiguration(HttpServletRequest 
+	 			request) {
+	 			 CorsConfiguration config = new CorsConfiguration();
+	 			 
+	 			config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+	 			 config.setAllowedMethods(Collections.singletonList("*"));
+	 			 config.setAllowCredentials(true);
+	 			 config.setAllowedHeaders(Collections.singletonList("*"));
+	 			 config.setExposedHeaders(Arrays.asList("Authorization"));
+	 			 config.setMaxAge(3600L);
+	 			 return config;
+	 			 }
+	 			 }).and()
+	          
+	          	          
+		    .authorizeHttpRequests()
+		    .requestMatchers("/api/all/**").hasAnyAuthority("ADMIN","USER")
+		    .requestMatchers("/api/getbyid/**").hasAnyAuthority("ADMIN","USER")
+		    .requestMatchers(HttpMethod.POST,"/api/addprod/**").hasAuthority("ADMIN")
+		    .requestMatchers(HttpMethod.PUT,"/api/updateprod/**").hasAuthority("ADMIN")
+		    .requestMatchers(HttpMethod.DELETE,"/api/delprod/**").hasAuthority("ADMIN")
+		    .requestMatchers(HttpMethod.POST,"/api/uplaodImageVoit/**").hasAuthority("ADMIN")
+		    .requestMatchers("/cat/**").hasAnyAuthority("ADMIN","USER")
+		    .anyRequest().authenticated().and()
+		    .addFilterBefore(new JWTAuthorizationFilter(), BasicAuthenticationFilter.class);
+
+		 return http.build();
+		
+	}
+	
+
+}
